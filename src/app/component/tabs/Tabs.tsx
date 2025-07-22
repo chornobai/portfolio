80226b40c45d610033488ec4b8694f127f6efea0
veronikachernobai <bombeze13@gmail.com>
2025-07-22 22:38:34 +0300

    fix

--- src/app/component/tabs/Tabs.tsx
+++ src/app/component/tabs/Tabs.tsx
@@ -1,136 +1,79 @@
 "use client"
-import * as React from "react";
-import {Tabs} from "radix-ui";
-import {FigmaIcon} from "@/app/assets/icons/FigmaIcon";
-import {MaterialIcon} from "@/app/assets/icons/MaterialIcon";
-import {TypeScriptIcon} from "@/app/assets/icons/TypeScriptIcon";
-import {GitIcon} from "@/app/assets/icons/GitIcon";
-import {HtmlIcon} from "@/app/assets/icons/HtmlIcon";
-import {JavaScriptIcon} from "@/app/assets/icons/JavaScriptIcon";
-import {ReactIcon} from "@/app/assets/icons/ReactIcon";
-import {ReduxIcon} from "@/app/assets/icons/ReduxIcon";
-import {SassIcon} from "@/app/assets/icons/SassIcon";
-import {SwaggerIcon} from "@/app/assets/icons/SwaggerIcon";
-import {StoryBookIcon} from "@/app/assets/icons/StoryBookIcon";
+import { Tabs } from "radix-ui"
+import Link from "next/link"
 
+
+import { FigmaIcon } from "@/app/assets/icons/FigmaIcon"
+import { MaterialIcon } from "@/app/assets/icons/MaterialIcon"
+import { TypeScriptIcon } from "@/app/assets/icons/TypeScriptIcon"
+import { GitIcon } from "@/app/assets/icons/GitIcon"
+import { HtmlIcon } from "@/app/assets/icons/HtmlIcon"
+import { JavaScriptIcon } from "@/app/assets/icons/JavaScriptIcon"
+import { ReactIcon } from "@/app/assets/icons/ReactIcon"
+import { ReduxIcon } from "@/app/assets/icons/ReduxIcon"
+import { SassIcon } from "@/app/assets/icons/SassIcon"
+import { SwaggerIcon } from "@/app/assets/icons/SwaggerIcon"
+import { StoryBookIcon } from "@/app/assets/icons/StoryBookIcon"
 
 const stacks = [
-    {
-        name: "Figma",
-        icon: <FigmaIcon/>,
-    },
-    {
-        name: "TypeScript",
-        icon: <TypeScriptIcon/>,
-    },
-    {
-        name: "Git",
-        icon: <GitIcon/>,
-    },
-    {
-        name: "Html",
-        icon: <HtmlIcon/>,
-    },
-    {
-        name: "JavaScript",
-        icon: <JavaScriptIcon/>,
-    },
-    {
-        name: "MUI",
-        icon: <MaterialIcon/>,
-    },
-    {
-        name: "React",
-        icon: <ReactIcon/>,
-    },
-    {
-        name: "Redux",
-        icon: <ReduxIcon/>,
-    },
-    {
-        name: "Sass",
-        icon: <SassIcon/>,
-    },
-    {
-        name: "Storybook",
-        icon: <StoryBookIcon/>,
-    },
-    {
-        name: "Swagger",
-        icon: <SwaggerIcon/>,
-    },
+    { name: "Figma", icon: <FigmaIcon /> },
+    { name: "TypeScript", icon: <TypeScriptIcon /> },
+    { name: "Git", icon: <GitIcon /> },
+    { name: "Html", icon: <HtmlIcon /> },
+    { name: "JavaScript", icon: <JavaScriptIcon /> },
+    { name: "MUI", icon: <MaterialIcon /> },
+    { name: "React", icon: <ReactIcon /> },
+    { name: "Redux", icon: <ReduxIcon /> },
+    { name: "Sass", icon: <SassIcon /> },
+    { name: "Storybook", icon: <StoryBookIcon /> },
+    { name: "Swagger", icon: <SwaggerIcon /> },
 ]
 
+const projectLinks = [
+    "https://smart-device-lac.vercel.app/",
+    "https://83892-keksobooking-23.vercel.app/",
+    "https://top-stroika.vercel.app/",
+    "https://bicycle-moon.vercel.app/",
+    "https://83892-sedona-22.vercel.app/",
+    "https://83892-cat-energy-22.vercel.app/",
+    "https://jewellery-five.vercel.app/",
+]
 
 const TabsProfile = () => (
-    <Tabs.Root
-        className="flex w-[100%] flex-col shadow-[0_2px_10px] shadow-blackA2"
-        defaultValue="tab1"
-    >
-        <Tabs.List
-            className="flex shrink-0 border-b border-mauve6"
-            aria-label="Manage your account"
-        >
-            <Tabs.Trigger
-                className="flex h-[45px] flex-1 cursor-default select-none items-center justify-center bg-white px-5 text-[15px] leading-none text-mauve11 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black"
-                value="tab1"
-            >
+    <Tabs.Root className="flex w-full flex-col shadow-[0_2px_10px] shadow-blackA2" defaultValue="tab1">
+        <Tabs.List className="flex border-b border-mauve6">
+            <Tabs.Trigger className="flex flex-1 items-center justify-center h-[45px] text-[15px] bg-white px-5 cursor-default select-none hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0]" value="tab1">
                 Stack
             </Tabs.Trigger>
-            <Tabs.Trigger
-                className="flex h-[45px] flex-1 cursor-default select-none items-center justify-center bg-white px-5 text-[15px] leading-none text-mauve11 outline-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black"
-                value="tab2"
-            >
+            <Tabs.Trigger className="flex flex-1 items-center justify-center h-[45px] text-[15px] bg-white px-5 cursor-default select-none hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0]" value="tab2">
                 Project
             </Tabs.Trigger>
         </Tabs.List>
-        <Tabs.Content
-            className=" bg-red-100 grow rounded-b-md  p-5 outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
-            value="tab1"
-        >
-            <ul className="grid grid-cols-[repeat(4,auto)] gap-4  ">
-                {stacks.map((item, index) => (
-                    <li key={index}
-                        className="px-[30px] py-[20px] flex flex-col items-center bg-white rounded-full gap-2 overflow-hidden">
-                        <div className="w-20 h-20">{item.icon}</div>
 
+        <Tabs.Content className="bg-red-100 rounded-b-md p-5" value="tab1">
+            <ul className="grid grid-cols-[repeat(4,auto)] gap-4">
+                {stacks.map((item, index) => (
+                    <li key={index} className="px-[30px] py-[20px] flex flex-col items-center bg-white rounded-full gap-2 overflow-hidden">
+                        <div className="w-20 h-20">{item.icon}</div>
                     </li>
                 ))}
             </ul>
         </Tabs.Content>
-        <Tabs.Content
-            className="bg-red-50 grow rounded-b-md  p-5 outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
-            value="tab2"
-        >
-            <ul className="flex flex-wrap">
 
-                <li>
-                    <iframe src="https://smart-device-lac.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-                <li>
-                    <iframe src="https://83892-keksobooking-23.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-                <li>
-                    <iframe src="https://top-stroika.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-                <li>
-                    <iframe src="https://bicycle-moon.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-                <li>
-                    <iframe src="https://83892-sedona-22.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-
-                <li>
-                    <iframe src="  https://83892-cat-energy-22.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
-
-                <li>
-                    <iframe src="https://jewellery-five.vercel.app/" width={"350"} height={"350"}></iframe>
-                </li>
+        <Tabs.Content className="bg-red-50 rounded-b-md p-5" value="tab2">
+            <ul className="flex flex-wrap gap-4">
+                {projectLinks.map((link, index) => (
+                    <li key={index} className="grow">
+                        <Link href={link} target="_blank" rel="noopener noreferrer" className="no-underline text-inherit">
+                            <div className="px-[20px] py-[20px] bg-white rounded-xl shadow-lg">
+                                <iframe src={link} width="100%" height="350" />
+                            </div>
+                        </Link>
+                    </li>
+                ))}
             </ul>
-
         </Tabs.Content>
     </Tabs.Root>
-);
+)
 
-export default TabsProfile;
+export default TabsProfile
